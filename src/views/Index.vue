a0ebd8d08935f067204221033fcc671b277743bc
Dannis <dannis@ariontechs.com>
2022-08-10 22:23:31 +0800

    fix: add NavItem & remove redundant  code

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <h1>Home</h1>
-    <button v-on="test">test</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomePage",
-  layout: 'SimpleLayout'
+  layout: 'SimpleLayout',
 };
 </script>
